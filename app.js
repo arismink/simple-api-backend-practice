9d3aa80db053e02a22583457bee3f6422e5d8f83
arismink <krismina.la@gmail.com>
2022-05-30 14:05:41 -0600

    add new get recipe route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+const mockData = require("./data.json");
+
 // simple GET request that returns a list of users
 // use /url to trigger callback return json data
 app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+})
+
+app.get("/recipes", (req, res, next) => {
+
+  res.json(mockData);
 })
 
 app.listen(3000, () => {
