15567af1c6448536b9478d60c4fd90540936e318
arismink <krismina.la@gmail.com>
2022-05-30 13:56:37 -0600

    set up simple GET request

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// simple GET request that returns a list of users
+// use /url to trigger callback return json data
+app.get("/url", (req, res, next) => {
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+})
+
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
