2be788d781d5e10e96f3ca913be2b187ec5018b4
arismink <krismina.la@gmail.com>
2022-05-30 18:57:43 -0600

    add recipe check logic

--- helpers/data-helpers.js
+++ helpers/data-helpers.js
@@ -3,11 +3,18 @@ const mockData = require("../data.json");
 const data = mockData.recipes
 
 const recipeCheck = (recipe) => {
+  if (data.some(d => d.name === recipe.name)) {
+    console.log("hello")
+    return false
+  }
 
+
+  saveRecipe(recipe)
+  return console.log("awesome!")
 }
 
 const saveRecipe = (recipe) => {
-
+  data.push(recipe);
 }
 
 module.exports = { recipeCheck, saveRecipe };
